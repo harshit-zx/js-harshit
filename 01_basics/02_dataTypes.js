ce2b5534c2d2d3619a894d15c7e901580d50e793
harshit-zx <153452035+harshit-zx@users.noreply.github.com>
2024-03-22 15:35:52 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //terat all js code as newer version 
+
+// alert (3+3) //we are using nodejs not browser 
+
+console.log ([3+3])
+
+let name = "harshit"
+let age = 21
+let isLoggedIn =false
+let state;
+
+// number => 2 to power 53
+// big intitise 
+// string => ""
+// boolean => true/fales
+// null => stand allone value 
+// undefined =>
+// symbol => unique  
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)  // OBJECT 
